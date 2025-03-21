8e5405c6d12e8cecac1b205638193153ce36ade5
Cristian-D-Pochettino <pochettinocristiandaniel@gmail.com>
2025-03-21 11:01:59 -0300

    modificaciones en preentrega

---
+++ Preentregas/Preentrega-1/main.js
@@ -0,0 +1,18 @@
+let nombre = prompt ("Ingrese su nombre para comenzar")
+
+    if(nombre == "Javier" && "Milton"){
+        alert("Bienvenido Profesor a la trivia Medieval")
+    }else if(nombre == "Milton"){
+        alert("Hola Miltooooooon!! Bienvenido a la trivia")
+    }else if(nombre ==""){
+        alert("Coloque bien su nombre, no sea pavote")
+    }else(nombre =="" || nombre == Number || nombre == null);{
+        alert("Usted no puede ingresar al sitio")}
+let pregunta1 = prompt ("1º pregunta= Quien fue William Wallace?")
+if(pregunta1 == "un caballero medieval"){
+    alert("Correcto! excelente respuesta")
+}else if(pregunta1 == "no se"){
+    alert("William Wallace fue un caballero escocés que lideró la lucha por la independencia de Escocia de Inglaterra a finales del siglo XIII y principios del XIV")
+}else if(pregunta1 == "Me importa un huevo"){
+    alert("Usted se tiene que arrepentir de lo que dijo, usted es un irrespetuoso")
+}
